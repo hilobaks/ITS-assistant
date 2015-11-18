fcaed17aea66b5992399723acea9cb8ae6b44e2f
Aleksandr Khylobokyi <Aleksandr Khilobokyi>
2015-11-19 01:54:30 +0200

    start config setting project

---
+++ src/AppBundle/Resources/myjs/indexPage.js
@@ -0,0 +1,22 @@
+/**
+ * Created by h_baks on 19.11.15.
+ */
+(function() {
+    var app = {
+
+        initialize : function () {
+            this.setUpListeners();
+        },
+
+        setUpListeners: function () {
+
+        },
+        eventHandlers : {
+
+        },
+        constant: {
+
+        }
+    };
+    app.initialize();
+}());
