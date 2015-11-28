c4493d948187250d113ce3dd3e5a5c490540fd6f
Aleksandr Khylobokyi <Aleksandr Khilobokyi>
2015-11-28 16:04:31 +0200

    add logic ip-calc, need test

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,6 +17,21 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    /**
+     * @Route("/registration", name="registration")
+     * @Method({"GET", "POST"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function registrationAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/registration.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
     }
