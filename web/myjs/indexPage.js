051f806b5ed7677c2501af425bcfeed325727ece
IrinaTim <ira_tim_4_enko@mail.ru>
2015-12-14 02:33:39 +0200

    v.1.0 app-ip-calc

--- web/myjs/indexPage.js
+++ web/myjs/indexPage.js
@@ -7,9 +7,7 @@
             this.setUpListeners.call(this.eventHandlers, this.eventElement);
         },
         setUpListeners: function (eventElement) {
-            EventUtil.addHandler(eventElement.inputMaskSubNetwork, 'click', this.selectTypeMask);
             EventUtil.addHandler(eventElement.ulApp, 'click', this.checkShowModal);
-            EventUtil.addHandler(eventElement.formCalcIP, 'submit', this.ipCalc);
         },
         eventHandlers : {
             checkShowModal: function (event) {
@@ -41,111 +39,12 @@
                         break;
                 }
             },
-            ipCalc: function (event) {
-                event = EventUtil.getEvent(event);
-                event.preventDefault(event);
-                var helpFunc = app.helpFunc,
-                    crossInnerText = helpFunc.crossInnerText,
-                    tenToSecond = helpFunc.tenToSecond,
-                    secondToTen = helpFunc.secondToTen,
-                    getNumberNetwork = helpFunc.getNumberNetwork;
-                var inputsIpAddress = document.querySelectorAll('.input-ip-address'),
-                    allIpAddress = {
-                        firstPartIp : crossInnerText(inputIpAddress[0], 'get'),
-                        secondPartIp : crossInnerText(inputIpAddress[1], 'get'),
-                        thirdPartIp : crossInnerText(inputIpAddress[2], 'get'),
-                        fourthPartIp : crossInnerText(inputIpAddress[3], 'get')
-                    };
-                var inputMask = document.querySelectorAll('.input-mask-subnetwork'),
-                    allMask = {
-                        firstPartMask : crossInnerText(inputMask[0], 'get'),
-                        secondPartMask : crossInnerText(inputMask[1], 'get'),
-                        thirdPartMask : crossInnerText(inputMask[2], 'get'),
-                        fourthPartMask : crossInnerText(inputMask[3], 'get')
-                    };
-                var outputNumberAddress = document.querySelectorAll('.output-ip-address'),
-                    allNumberAddress = {
-                        firstPartNumberAddress : outputNumberAddress[0],
-                        secondPartNumberAddress : outputNumberAddress[1],
-                        thirdPartNumberAddress : outputNumberAddress[2],
-                        fourthPartNumberAddress : outputNumberAddress[3]
-                    };
-                var ipTenToSecond = tenToSecond(allIpAddress).join(''),
-                    maskTenToSecond = tenToSecond(allMask).join('');
-                var numberNetwork = getNumberNetwork(ipTenToSecond, maskTenToSecond),
-                    response = secondToTen(numberNetwork.match(/\d{4}/));
-                for(var field in allNumberAddress) {
-                    crossInnerText(field, 'set', response[0]);
-                    response.shift();
-                }
-            },
-            selectTypeMask: function (event) {
-                event = EventUtil.getEvent(event);
-                var target = EventUtil.getTarget(event);
-                if(target.classList.contains('input-mask-subnetwork')) {
-                    var inputMaskSubNetwork = document.querySelectorAll('.input-mask-subnetwork'),
-                        idNumberOneInMask = 'number-one-in-mask',
-                        numberOneInMask = document.getElementById(idNumberOneInMask),
-                        addRequiredForElement = function (boolValue) {
-                            //inputMaskSubnetwork.length - 2 - все кроме последнего
-                            for(var i = 0; i <= inputMaskSubNetwork.length - 2; i++) {
-                                inputMaskSubNetwork[i].required = boolValue;
-                        }
-                    };
-                    if(target.getAttribute('id') === idNumberOneInMask) {
-                        target.required = true;
-                        addRequiredForElement(false);
-                    } else {
-                        addRequiredForElement(true);
-                        numberOneInMask.required = false;
-                    }
-                }
-            }
         },
         eventElement: {
-            ulApp : document.querySelector('#open-app-button'),
-            formCalcIP : document.querySelector('#form-calc-IP'),
-            inputMaskSubNetwork : document.getElementById('mask-sub-network')
+            ulApp : document.querySelector('#open-app-button')
         },
         helpFunc: {
-            showModal: function (modalWindow) {
-                var CalcModal = document.querySelector(modalWindow);
-                $(CalcModal).modal('show');
-            },
-            tenToSecond: function (valueInTen) {
-                var ipAddressInTwo = [];
-                for(var field in valueInTen) {
-                    ipAddressInTwo.push(valueInTen[field].toString(2));
-                }
-                return ipAddressInTwo;
-            },
-            secondToTen: function (valueInSecond) {
-                var numberAddress = [];
-                for(var field in valueInSecond) {
-                    var partNumberAddress = [];
-                    partNumberAddress.push(parseInt(valueInSecond[field], 10));
-                    numberAddress.push(partNumberAddress);
-                }
-                return numberAddress;
-            },
-            getNumberNetwork: function (ipAddress, maskSubNetwork) {
-                var numberNetwork = [];
-                for(var i = 0; i != ipAddress.length - 1; i++ ) {
-                    numberNetwork.push((Number(ipAddress[i]) & Number(maskSubNetwork[i])).toString());
-                }
-                return numberNetwork;
-            },
-            crossInnerText: function (elem, action, text) {
-                if(action === 'set') {
-                    if(elem.innerText) {
-                        elem.innerText = text;
-                    } else if(elem.textContent) {
-                        elem.textContent = text;
-                    }
-                } else if(action === 'get') {
-                    return elem.innerText || elem.textContent;
-                }
-            }
+
         }
     };
     app.initialize();
--- web/myjs/ip-calc.js
+++ web/myjs/ip-calc.js
@@ -1,20 +1,103 @@
 (function() {
     var app = {
         initialize : function () {
-            this.setUpListeners.call(this.eventHandlers);
+            this.setUpListeners.call(this.eventHandlers, this.eventElements);
         },
 
-        setUpListeners: function () {
-
+        setUpListeners: function (eventElements) {
+            EventUtil.addHandler(eventElements.inputMaskSubNetwork, 'click', this.selectTypeMask);
+            EventUtil.addHandler(eventElements.formCalcIP, 'submit', this.ipCalc);
         },
         eventHandlers : {
-
+            ipCalc: function (event) {
+                event = EventUtil.getEvent(event);
+                event.preventDefault(event);
+                var inputsIpAddress = this.elements['input-ip-address'],
+                    allIpAddress = {
+                        firstPartIp : inputsIpAddress[0].value,
+                        secondPartIp : inputsIpAddress[1].value,
+                        thirdPartIp : inputsIpAddress[2].value,
+                        fourthPartIp : inputsIpAddress[3].value
+                    };
+                var inputsMask = this.elements['input-mask-subnetwork'],
+                    allMask = {
+                        firstPartMask : inputsMask[0].value,
+                        secondPartMask : inputsMask[1].value,
+                        thirdPartMask : inputsMask[2].value,
+                        fourthPartMask : inputsMask[3].value
+                    };
+                var outputNumberAddress = this.elements['output-ip-address'],
+                    allNumberAddress = {
+                        firstPartNumberAddress : outputNumberAddress[0],
+                        secondPartNumberAddress : outputNumberAddress[1],
+                        thirdPartNumberAddress : outputNumberAddress[2],
+                        fourthPartNumberAddress : outputNumberAddress[3]
+                    };
+                var ipTenToSecond = app.helpFunc.tenToSecond(allIpAddress).join(''),
+                    maskTenToSecond = app.helpFunc.tenToSecond(allMask).join('');
+                var numberNetwork = app.helpFunc.getNumberNetwork(ipTenToSecond, maskTenToSecond),
+                    response = app.helpFunc.secondToTen(numberNetwork.match(/\d{8}/g));
+                for(var field in allNumberAddress) {
+                    allNumberAddress[field].value = response[0];
+                    response.shift();
+                }
+                forAllPage.funcS.animateOpacity(0, 1, document.querySelector('#output-ip-addresses'));
+            },
+            selectTypeMask: function (event) {
+                event = EventUtil.getEvent(event);
+                var target = EventUtil.getTarget(event);
+                if(target.classList.contains('input-mask-subnetwork')) {
+                    var inputMaskSubNetwork = document.querySelectorAll('.input-mask-subnetwork'),
+                        idNumberOneInMask = 'number-one-in-mask',
+                        numberOneInMask = document.getElementById(idNumberOneInMask),
+                        addRequiredForElement = function (boolValue) {
+                            //inputMaskSubnetwork.length - 2 - все кроме последнего
+                            for(var i = 0; i <= inputMaskSubNetwork.length - 2; i++) {
+                                inputMaskSubNetwork[i].required = boolValue;
+                            }
+                        };
+                    if(target.getAttribute('id') === idNumberOneInMask) {
+                        target.required = true;
+                        addRequiredForElement(false);
+                    } else {
+                        addRequiredForElement(true);
+                        numberOneInMask.required = false;
+                    }
+                }
+            }
         },
-        eventsElement: {
-
+        eventElements: {
+            formCalcIP : document.querySelector('#form-calc-IP'),
+            inputMaskSubNetwork : document.getElementById('mask-sub-network')
         },
         helpFunc: {
-
+            tenToSecond: function (valueInTen) {
+                var ipAddressInTwo = [];
+                for(var field in valueInTen) {
+                    var tempNumber = Number(valueInTen[field]).toString(2);
+                    if(tempNumber.length !== 8) {
+                        for(var i = 0; tempNumber.length != 8; i++ ) tempNumber +=  '0';
+                    }
+                    ipAddressInTwo.push(tempNumber);
+                }
+                return ipAddressInTwo;
+            },
+            secondToTen: function (valueInSecond) {
+                var numberAddress = [];
+                for(var field in valueInSecond) {
+                    var partNumberAddress = [];
+                    partNumberAddress.push(parseInt(valueInSecond[field], 2));
+                    numberAddress.push(partNumberAddress);
+                }
+                return numberAddress;
+            },
+            getNumberNetwork: function (ipAddress, maskSubNetwork) {
+                var numberNetwork = [];
+                for(var i = 0; i < ipAddress.length ; i++ ) {
+                    numberNetwork.push((Number(ipAddress[i]) & Number(maskSubNetwork[i])).toString());
+                }
+                return numberNetwork.join('');
+            }
         },
         helpVar: {
 
